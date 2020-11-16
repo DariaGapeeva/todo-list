42526f806de8385ec76a2bedc4bf8d3747ef5003
DariaGapeeva <d.gapeeva@gmail.com>
2020-11-16 22:47:31 +0700

    Updates

---
+++ precache-manifest.730891188c67c756703733ede0a6166d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b997b31208554cd152b2b2b0fe19d137",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "8b6ace702d88c35dbad4",
+    "url": "/todo-list/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "16100d2cf4a25bfd52e1",
+    "url": "/todo-list/static/js/2.81f4fc7d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/todo-list/static/js/2.81f4fc7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6ace702d88c35dbad4",
+    "url": "/todo-list/static/js/main.888afe2d.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
